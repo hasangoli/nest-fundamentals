6ced29e017dfb6eab390130d459c8b889f6937bc
Hasan Goli <hasangolidev@gmail.com>
2024-02-22 19:56:57 +0330

    Interceptor added

---
+++ src/common/interceptors/wrap-response/wrap-response.interceptor.ts
@@ -0,0 +1,21 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+
+@Injectable()
+export class WrapResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    // console.log('Before...');
+
+    return next.handle().pipe(
+      map((data: any): { useInterceptor: boolean; data: any } => ({
+        useInterceptor: true,
+        data,
+      })),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor';
 
 async function bootstrap(): Promise<void> {
   const app: INestApplication<any> = await NestFactory.create(AppModule);
@@ -20,6 +21,8 @@ async function bootstrap(): Promise<void> {
   app.useGlobalFilters(new HttpExceptionFilter());
 
   // app.useGlobalGuards(new ApiKeyGuard());
+
+  app.useGlobalInterceptors(new WrapResponseInterceptor());
 
   await app.listen(3000);
 }
